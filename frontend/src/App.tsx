c8af8f165f8ceb6b4e0a9a4f9faeb0bd4ce46498
shAn-kor <kjn0406@naver.com>
2025-09-09 00:44:08 +0900

    javascript -> typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>E-Commerce Frontend</h1>
+        <p>Vite + React + TypeScript</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
